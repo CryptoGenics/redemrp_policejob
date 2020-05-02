0e860c63a85c3a7938fe0a0210e733d4e531ad11
CryptoGenics <45253987+CryptoGenics@users.noreply.github.com>
2020-05-02 10:04:50 -0500

    initial commit

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.Offices = {
+    { x = -278.75, y = 808.73, z = 118.58, name = 'val'}, --val
+    { x = -764.91, y = -1271.96, z = 43.85, name = 'black' },  --blackwater
+    { x = 1361.12, y = -1304.57, z = 77.77, name = 'rhodes' },  --rhodes
+    { x = -1813.57, y = -354.83, z = 164.65, name = 'strawberry' },  --strawberry
+    { x = -5527.06, y = -2928.43, z = -1.36, name = 'tumbleweek' },  --tumbleweed
+    { x = -3625.28, y = -2601.66, z = -13.34, name = 'armadillo' },  --armadillo
+    { x = 2906.44, y = 1314.93, z = 44.94, name = 'annesburg' },  --annesburg
+    { x = 2496.79, y = -1302.06, z = 48.95, name = 'saint' },  --saint
+}
